f1f2fc0e3c9db4ca47ef8bb4d8ebb7e5b7d3af23
Ignasi Marimon-Clos <ignasi.marimon@agilogy.com>
2014-05-31 17:51:33 +0200

    Provides data for lastevent, and next event

--- app/actors/BroadcastActor.scala
+++ app/actors/BroadcastActor.scala
@@ -2,13 +2,13 @@ package actors
 
 import play.api.libs.iteratee.Concurrent
 import akka.actor.{Props, Actor}
-
+import play.api.libs.json._
 
 object BroadcastActor {
 
   case class Subscribe(endpoint: Concurrent.Channel[String])
 
-  case class Publish(msg: String)
+  case class Publish(json: JsValue)
 
   case object Refresh
 
@@ -23,7 +23,7 @@ class BroadcastActor extends Actor {
   override def receive: Receive = {
     case BroadcastActor.Subscribe(endpoint) => endpoints = endpoint :: endpoints
     case BroadcastActor.Publish(v) => endpoints.foreach {
-      _.push(v)
+      _.push(Json.stringify(v))
     }
     case BroadcastActor.Refresh => {
       eventsActor ! MeetupsActor.Refresh
