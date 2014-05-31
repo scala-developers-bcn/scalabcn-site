2a75b33bf343063b717f47f8d7cedb45175f772d
Ignasi Marimon-Clos <ignasi.marimon@agilogy.com>
2014-05-31 11:02:07 +0200

    Adds WS route and endpoint

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,26 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.iteratee.{Enumerator, Iteratee}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def ws = WebSocket.using[String] { request =>
+
+    // Log events to the console
+    val in = Iteratee.foreach[String](println).map { _ =>
+      println("Disconnected")
+    }
+
+    // Send a single 'Hello!' message
+    val out = Enumerator("Hello!")
+
+    (in, out)
   }
 
 }
