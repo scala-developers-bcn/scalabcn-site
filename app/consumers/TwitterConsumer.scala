d990b6c6be427898508881fdd46203b550dd3617
Ignasi Marimon-Clos <ignasi.marimon@agilogy.com>
2014-05-31 15:59:27 +0200

    Parses twitter JSON and reduces frequency

--- app/consumers/TwitterConsumer.scala
+++ app/consumers/TwitterConsumer.scala
@@ -12,11 +12,11 @@ import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.DefaultHttpClient
 import org.apache.commons.io.IOUtils
 import org.apache.http.params.HttpParams
-import scala.concurrent.{ Future, ExecutionContext }
+import scala.concurrent.{Future, ExecutionContext}
 import rx.lang.scala.subjects.ReplaySubject
 import scala.util.Success
 import scala.util.Failure
-import scala.concurrent.{ Future, ExecutionContext }
+import scala.concurrent.{Future, ExecutionContext}
 import scala.concurrent.ExecutionContext.Implicits.global
 
 case class QueryTopicResponse(topic: String, statusCode: Int, content: String)
@@ -26,32 +26,32 @@ class TwitterConsumer {
   private val consumerSecret = System.getenv("TWITTER_CONSUMER_SECRET")
   private val accessToken = System.getenv("TWITTER_ACCESS_TOKEN")
   private val accessTokenSecret = System.getenv("TWITTER_ACCESS_TOKEN_SECRET")
-  
+
   private val consumer = new CommonsHttpOAuthConsumer(consumerKey, consumerSecret);
   consumer.setTokenWithSecret(accessToken, accessTokenSecret);
   private val httpClient = new DefaultHttpClient();
-  
+
   def queryTopic(topic: String, amount: Int) = {
     println(s"queryTopic $topic $amount")
     val request = new HttpGet(s"https://api.twitter.com/1.1/search/tweets.json?q=$topic&count=$amount");
     consumer.sign(request);
     val response = httpClient.execute(request);
-    QueryTopicResponse(topic, 
-        response.getStatusLine().getStatusCode(), 
-        IOUtils.toString(response.getEntity().getContent()))
-  } 
+    QueryTopicResponse(topic,
+      response.getStatusLine().getStatusCode(),
+      IOUtils.toString(response.getEntity().getContent()))
+  }
 }
 
 object TwitterConsumer {
-  
-  def observable(topic: String, amount: Int) = {
+
+  def observable(topic: String, amount: Int):Observable[QueryTopicResponse] = {
     val twitterConsumer = new TwitterConsumer
-    Observable.interval(500 millis).map(t => twitterConsumer.queryTopic(topic, amount))
-  } 
-  
+    Observable.timer(0 seconds, 30 seconds).map(t => twitterConsumer.queryTopic(topic, amount))
+  }
+
   def main(args: Array[String]): Unit = {
     //Create an observable on a specific topic
-    val o = TwitterConsumer.observable("scalabcn", 5) 
+    val o = TwitterConsumer.observable("scalabcn", 5)
     val subscription = o.subscribe((resp) => println(resp), (e) => println(e))
     Thread.sleep(20000);
   }
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,6 +12,8 @@ import scala.concurrent.{ Channel, Promise }
 import consumers.TwitterConsumer
 import akka.actor.ActorRef
 
+import play.api.libs.json._
+
 object RandomActor {
 
   case class Subscribe(endpoint: Concurrent.Channel[String])
@@ -27,10 +29,16 @@ class RandomActor extends Actor {
     case RandomActor.Subscribe(endpoint) => endpoints = endpoint :: endpoints
     case RandomActor.Publish(v) => endpoints.foreach { _.push(v) }
   }
+}
 
-  context.system.scheduler.schedule(1 second, 1 second) {
-    self ! RandomActor.Publish("" + Random.nextInt())
-  }
+
+ case class Status(text:String)
+  case class Statuses(statuses:List[Status])
+object TwitterProtocol{
+
+  implicit def StatusFormat =Json.format[Status]
+  implicit def StatusesFormat =Json.format[Statuses]
+
 }
 
 class TwitterConsumerActor(broadcastActorRef: ActorRef) extends Actor {
@@ -39,12 +47,22 @@ class TwitterConsumerActor(broadcastActorRef: ActorRef) extends Actor {
     (response) => {
       val status = response.statusCode
       println(s"TwitterConsumerActor: performed request ($status)")
-      broadcastActorRef ! RandomActor.Publish(response.content)
+      import TwitterProtocol._
+
+      val jsResult:JsResult[Statuses] =  Json.fromJson[Statuses](
+        Json.parse(response.content)
+      )
+
+      jsResult map { statuses =>
+          val humanReadableMessages:String = statuses.statuses.map{_.text}.mkString("<br/>")
+          broadcastActorRef ! RandomActor.Publish(humanReadableMessages)
+      }
+
     },
     (error) => System.err.println(s"TwitterConsumerActor: $error"),
     () => println(s"TwitterConsumerActor: scala bcn observable completed")
   )
-  
+
   override def receive: Receive = {
     case _ => println(s"TwitterConsumerActor: Oh, oh, what to do, what to do...")
   }
