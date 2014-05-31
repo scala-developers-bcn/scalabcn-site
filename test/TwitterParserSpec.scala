48543d5a72a29c6890c9e0c7f7dec2e48364a9bc
Ignasi Marimon-Clos <ignasi.marimon@agilogy.com>
2014-05-31 20:07:38 +0200

    Test

--- test/TwitterParserSpec.scala
+++ test/TwitterParserSpec.scala
@@ -1,6 +1,24 @@
-/**
- * Created by ignasi on 31/05/14.
- */
-class TwitterParserSpec {
+import org.specs2.mutable.Specification
+import play.api.libs.json.Json
+import scala.io.Source
+
+class TwitterParserSpec extends Specification {
+
+  "A TwitterParser" should {
+    "extract the picture from a json-tweet" in {
+      val in = getClass.getResourceAsStream("/samples/twitter/tweet_with_photo.json")
+      in should not equalTo(null)
+
+      val twitAsString = Source.fromInputStream(in).getLines().toList.mkString
+
+      TwitterParser.photosOf(twitAsString) should equalTo(List("http://pbs.twimg.com/media/Bo-diCyCQAME2Ic.jpg"))
+    }
+  }
+}
+
+object TwitterParser {
+  def photosOf(s: String) = {
+    Json.parse(s) \ "entities" \ "media" \ "media_url"
+  }
 
 }
