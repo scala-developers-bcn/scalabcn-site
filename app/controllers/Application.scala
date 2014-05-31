c7b88f7b190d463962a2a22db765854a2e366fe9
Ignasi Marimon-Clos <ignasi.marimon@agilogy.com>
2014-05-31 12:47:30 +0200

    Adds actor-based implementation of random number generation

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,16 +2,40 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-import play.api.libs.iteratee.{Enumerator, Iteratee}
+import play.api.libs.iteratee.{Concurrent, Enumerator, Iteratee}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
 import scala.util.Random
+import akka.actor.{Props, Actor}
+import play.libs.Akka
+import scala.concurrent.{Channel, Promise}
+
+
+object RandomActor {
+  case class Subscribe(endpoint: Concurrent.Channel[String])
+  case object Publish
+}
+
+class RandomActor extends Actor {
+  var endpoint: Concurrent.Channel[String] = null;
+
+  override def receive: Receive = {
+    case RandomActor.Subscribe(endpoint) => this.endpoint = endpoint
+    case RandomActor.Publish => {
+      endpoint.push("" + Random.nextInt())
+    }
+  }
+
+  context.system.scheduler.schedule(1 second, 1 second) {
+    self ! RandomActor.Publish
+  }
+}
 
 object Application extends Controller {
+  def index = Action { implicit request => Ok(views.html.index()) }
 
-
-  def index = Action { implicit request =>
-    Ok(views.html.index())
-  }
+  val actor = Akka.system.actorOf(Props(classOf[RandomActor]))
 
 
   def ws = WebSocket.using[String] { request =>
@@ -21,8 +45,12 @@ object Application extends Controller {
       println("Disconnected")
     }
 
-    // Send a single 'Hello!' message
-    val out = Enumerator(""  + Random.nextInt())
+
+    val broadcast: (Enumerator[String], Concurrent.Channel[String]) = Concurrent.broadcast[String]
+    actor ! RandomActor.Subscribe(broadcast._2)
+
+    val out = broadcast._1
+
 
     (in, out)
   }
